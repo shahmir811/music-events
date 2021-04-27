1929e75fef0c2e221f6bbfd26873d6161c6472e9
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-04-27 11:07:20 +0500

    Added AuthContext. Completed User Authentication work

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext, Fragment } from 'react';
 import Link from 'next/link';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
+import AuthContext from '@/context/AuthContext';
 import Search from './Search';
 import styles from '@/styles/Header.module.css';
 
 const Header = () => {
+	const { user, logout } = useContext(AuthContext);
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.logo}>
@@ -22,11 +26,40 @@ const Header = () => {
 							<a>Events</a>
 						</Link>
 					</li>
-					<li>
-						<Link href='/events/add'>
-							<a>Add Event</a>
-						</Link>
-					</li>
+					{user ? (
+						// If the user is logged In
+						<Fragment>
+							<li>
+								<Link href='/events/add'>
+									<a>Add Event</a>
+								</Link>
+							</li>
+							<li>
+								<Link href='/account/dashboard'>
+									<a>Dashboard</a>
+								</Link>
+							</li>
+							<li>
+								<button
+									className='btn-secondary btn-icon'
+									onClick={() => logout()}
+								>
+									<FaSignOutAlt /> Logout
+								</button>
+							</li>
+						</Fragment>
+					) : (
+						// If the user logged Out
+						<Fragment>
+							<li>
+								<Link href='/account/login'>
+									<a className='btn-secondary btn-icon'>
+										<FaSignInAlt /> Login
+									</a>
+								</Link>
+							</li>
+						</Fragment>
+					)}
 				</ul>
 			</nav>
 		</header>
---
+++ pages/api/user.js
@@ -0,0 +1,37 @@
+import cookie from 'cookie';
+import { API_URL } from '@/config/index';
+
+export default async (req, res) => {
+	if (req.method === 'GET') {
+		// if no token in headers cookie
+		if (!req.headers.cookie) {
+			res.status(403).json({
+				message: 'Not authorized',
+			});
+			return;
+		}
+
+		// parse token
+		const { token } = cookie.parse(req.headers.cookie);
+
+		const strapiRes = await fetch(`${API_URL}/users/me`, {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const user = await strapiRes.json();
+
+		if (strapiRes.ok) {
+			res.status(200).json({ user });
+		} else {
+			res.status(403).json({
+				message: 'User forbidden',
+			});
+		}
+	} else {
+		res.setHeader('Allow', ['GET']);
+		res.status(405).json({ message: `Method ${req.method} not allowed` });
+	}
+};
